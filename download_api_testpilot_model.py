f49934272a578169e60fb503e5db5f3a97150bef
walker <yangmeai@163.com>
2025-04-13 10:00:56 +0800

    Update download_api_testpilot_model.py

--- download_api_testpilot_model.py
+++ download_api_testpilot_model.py
@@ -1,4 +1,4 @@
 from modelscope import snapshot_download
 
 # 请一定先按照操作步骤安装依赖之后再执行此脚本
-model_dir = snapshot_download('walker001/api-testpilot-model')
+model_dir = snapshot_download('walker001/api-testpilot-model-v1.2')
